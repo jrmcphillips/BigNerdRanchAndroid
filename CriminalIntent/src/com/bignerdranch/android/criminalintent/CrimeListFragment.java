430485fbcea91058e7b4cc0f157688a905b70fdc
Jonathan McPhillips <jrmcphillips@gmail.com>
2013-08-13 23:16:44 -0700

    CriminalIntent: fixed fragment view

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.*;
 import android.util.*;
 import java.util.*;
+import android.view.*;
 
 public class CrimeListFragment extends ListFragment {
 	private CrimeLab mCrimeLab;
@@ -27,9 +28,11 @@ public class CrimeListFragment extends ListFragment {
 	public void onListItemClick(ListView listView, View v, int position, long id) {
 		Crime crime = (Crime) getListAdapter().getItem(position);
 		Log.i(this.getClass().getSimpleName(), "" + position);
-		Toast.makeText(listView.getContext().getApplicationContext(), "item: " + position, Toast.LENGTH_SHORT).show();
+		Toast.makeText(getActivity(), "item: " + position, Toast.LENGTH_SHORT).show();
 		crime.setTitle(crime.isSolved() + "");
 	};
+	
+	
 	
 	private class CrimeAdapter extends ArrayAdapter<Crime> {
 		public CrimeAdapter(CrimeMap crimeMap) {
@@ -37,6 +40,26 @@ public class CrimeListFragment extends ListFragment {
 				getActivity(),
 				R.layout.list_item_crime,
 				new ArrayList(crimeMap.values()));
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup group) {
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+			}
+
+			Crime crime = getItem(position);
+			
+			TextView titleTextView = (TextView) convertView.findViewById(R.id.crime_list_item_title_text_view);
+			titleTextView.setText(crime.getTitle());
+			
+			TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_date_text_view);
+			dateTextView.setText(crime.getDate().toString());
+			
+			TextView crimeCheckBox = (CheckBox) convertView.findViewById(R.id.crime_list_item_solved_check_box);
+			crimeCheckBox.setSelected(crime.isSolved());
+			
+			return convertView;
 		}
 	}
 }
