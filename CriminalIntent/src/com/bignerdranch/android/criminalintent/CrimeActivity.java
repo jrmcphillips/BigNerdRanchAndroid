bf642b50c5eace4f8d5ed4a83fa64bb4527bee68
Jonathan McPhillips <jrmcphillips@gmail.com>
2013-12-15 15:26:33 -0600

    applied formatting changes

--- CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,13 +1,13 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.os.*;
-import android.support.v4.app.*;
+import android.support.v4.app.Fragment;
 
 public class CrimeActivity extends SingleFragmentActivity {
-	protected Fragment createFragment() {
-		
-		Crime crime = getIntent().getParcelableExtra(CrimeFragment.CRIME_KEY);
-		return CrimeFragment.newInstance(crime);
-	}
-}
+    @Override
+    protected Fragment createFragment() {
 
+        final Crime crime = getIntent().getParcelableExtra(
+                CrimeFragment.CRIME_KEY);
+        return CrimeFragment.newInstance(crime);
+    }
+}
